d1d5f2492ec019190d2b313797b241ad40bcd295
Edgar Fernandez <tempurakohi@gmail.com>
2022-02-13 02:24:17 -0800

    seeing if commit works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,8 @@
 const router= require ("express").Router()
-const store = require("../db/save")
-// const save= require("../db/store")
+const save = require("../db/save")
 
 router.get("/notes",(req,res)=>{
-    store.readNotes()
+    save.readNotes()
     .then((notes)=> {
         return res.json(notes)
     })
@@ -11,13 +10,13 @@ router.get("/notes",(req,res)=>{
 })
 
 router.post("/notes",(req,res)=> {
-    store.createNote(req.body)
+    save.createNote(req.body)
     .then((note)=> res.json(note))
     .catch((err)=> res.status(500).json(err))
 })
 
 router.delete("/notes/:id", (req,res)=> {
-    store.deleteNote(req.params.id)
+    save.deleteNote(req.params.id)
     .then(()=> res.json({ ok:true }))
     .catch((err)=> res.status(500).json(err))
 })
